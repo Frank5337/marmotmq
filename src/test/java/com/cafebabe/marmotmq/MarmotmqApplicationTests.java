a019c1c9cd8ae04a664b8bd5cb4e63462acca713
陈·风暴烈酒 <1055353746@qq.com>
2019-09-30 21:51:04 +0800

    init 因为需要一个gui管理界面，所以做成一个web项目；除springMVC外，本项目不再依赖任何第三方框架和组件

---
+++ src/test/java/com/cafebabe/marmotmq/MarmotmqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cafebabe.marmotmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarmotmqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
